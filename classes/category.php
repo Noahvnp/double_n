1fb3f211f2f59f665654c93fca398f12e96ecce7
Noahvnp <norahpvn@gmail.com>
2022-04-21 04:17:15 +0700

    Thêm chức năng xoá, 10

---
+++ classes/category.php
@@ -0,0 +1,89 @@
+<?php
+    include '../lib/database.php';
+    include '../helpers/format.php';
+?>
+
+<?php
+    class category
+    {
+        private $db;
+        private $fm;
+
+        public function __construct()
+        {
+            $this->db = new Database();
+            $this->fm = new Format();
+        }
+
+        public function insert_category($catName){
+            $catName = $this->fm->validation($catName);
+
+            $catName = mysqli_real_escape_string($this->db->link, $catName);
+
+            if (empty($catName)) {
+                $loginmsg = "Category must not be empty!";
+                return $loginmsg;
+            } else {
+                $query = "INSERT INTO tbl_category(catName) VALUES('$catName')";
+                $result = $this->db->insert($query);
+
+                if($result){
+                    $alert = "<span class='success'>Category inserted successfully!</span>";
+                    return $alert;
+                } else {
+                    $alert = "<span class='error'>Category not inserted!</span>";
+                    return $alert;
+                }
+            }
+        }
+
+        public function show_category(){
+            $query = "SELECT * FROM tbl_category ORDER BY catId DESC";
+            $result = $this->db->select($query);
+
+            return $result;
+        }
+
+        public function update_category($catName, $catId){
+            $catName = $this->fm->validation($catName);
+
+            $catName = mysqli_real_escape_string($this->db->link, $catName);
+            $catId = mysqli_real_escape_string($this->db->link, $catId);
+
+            if (empty($catName)) {
+                $msg = "<span class='error'>Category must not be empty!>";
+                return $msg;
+            } else {
+                $query = "UPDATE tbl_category SET catName = '$catName' WHERE catId = '$catId'";
+                $result = $this->db->update($query);
+
+                if($result){
+                    $alert = "<span class='success'>Category updated successfully!</span>";
+                    return $alert;
+                } else {
+                    $alert = "<span class='error'>Category not updated!</span>";
+                    return $alert;
+                }
+            }
+        }
+
+        public function del_category($id){
+            $query = "DELETE FROM tbl_category WHERE catId = '$id'";
+            $result = $this->db->delete($query);
+            if($result){
+                $alert = "<span class='success'>Category deleted successfully!</span>";
+                return $alert;
+            } else {
+                $alert = "<span class='error'>Category not deleted!</span>";
+                return $alert;
+            }
+        }        
+
+        public function getcatbyId($id){
+            $query = "SELECT * FROM tbl_category WHERE catId = '$id'";
+            $result = $this->db->select($query);
+            
+            return $result;
+        }
+    }
+?>
---
+++ double_n.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 20, 2022 lúc 04:56 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `double_n`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `adminId` int(11) NOT NULL,
+  `adminName` varchar(255) NOT NULL,
+  `adminEmail` varchar(150) NOT NULL,
+  `adminUser` varchar(255) NOT NULL,
+  `adminPass` varchar(255) NOT NULL,
+  `level` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`adminId`, `adminName`, `adminEmail`, `adminUser`, `adminPass`, `level`) VALUES
+(1, 'nam', 'nam@gmail.com', 'namadmin', 'e10adc3949ba59abbe56e057f20f883e', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `catId` int(11) NOT NULL,
+  `catName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`catId`, `catName`) VALUES
+(1, 'Asus'),
+(2, 'Dell'),
+(3, 'Accessories'),
+(4, 'Clothing &amp; Accessories');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`adminId`);
+
+--
+-- Chỉ mục cho bảng `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`catId`) USING BTREE;
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `catId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
