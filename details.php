64da31c0f96ad90d6d6efd21e41f810b2a3598a5
Namphuongle <lephuongnam11062001@gmail.com>
2022-04-22 22:55:59 +0700

    xong b25

--- details.php
+++ details.php
@@ -1,6 +1,6 @@
 <?php
 	include 'inc/header.php';
-	include 'inc/slider.php';
+	//include 'inc/slider.php';
 ?>
 
 <?php
@@ -8,6 +8,10 @@ if(!isset($_GET['proid']) || $_GET['proid'] == NULL){
 	echo "<script>window.location = '404.php';</script>";
 } else {
 	$id = $_GET['proid'];
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+	$quantity = $_POST['quantity'];
+	$AddtoCart = $ct->add_to_cart($quantity, $id);
 }
 ?>
 
@@ -26,7 +30,7 @@ if(!isset($_GET['proid']) || $_GET['proid'] == NULL){
 
 				<div class="cont-desc span_1_of_2">				
 					<div class="grid images_3_of_2">
-						<img src="images/preview-img.jpg" alt="" />
+						<img src="admin/uploads/<?php echo $result_details['image'] ?>" alt="" />
 					</div>
 				<div class="desc span_3_of_2">
 					<h2><?php echo $result_details['productName'] ?> </h2>
@@ -38,10 +42,16 @@ if(!isset($_GET['proid']) || $_GET['proid'] == NULL){
 						<p>Brand:<span><?php echo $result_details['brandName'] ?> </span></p>
 					</div>
 				<div class="add-cart">
-					<form action="cart.php" method="post">
-						<input type="number" class="buyfield" name="" value="1"/>
+					<form action="" method="post">
+						<input type="number" class="buyfield" name="quantity" value="1" min="1"/>
 						<input type="submit" class="buysubmit" name="submit" value="Buy Now"/>
-					</form>				
+						
+					</form>
+					<?php
+						if(isset($AddtoCart)){
+							echo '<span style="color:red;font-size:18px;">Product Already Added</span>';
+						}
+						?>				
 				</div>
 			</div>
 			<div class="product-desc">
