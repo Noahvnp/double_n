d76c8ae8601d865e08a2ba62ea94c254e2f593d2
Noahvnp <norahpvn@gmail.com>
2022-04-22 07:15:29 +0700

    update đường dẫn file trong b15

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
-﻿<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
+﻿<?php include './inc/header.php';?>
+<?php include './inc/sidebar.php';?>
         <div class="grid_10">
             <div class="box round first grid">
                 <h2> Dashbord</h2>
@@ -8,4 +8,4 @@
                 </div>
             </div>
         </div>
-<?php include 'inc/footer.php';?>
+<?php include './inc/footer.php';?>
